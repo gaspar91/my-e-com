3373f47b8636eb8fa97498cc55e0ce4faab1f597
gaspar91 <andreiafg.91@gmail.com>
2021-01-17 09:35:46 +0000

    created the edit product template, view and url

--- products/views.py
+++ products/views.py
@@ -78,12 +78,12 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully added product!')
+            messages.success(request, 'Your product was successfully added!')
             return redirect(reverse('add_product'))
         else:
             messages.error(
                 request,
-                'Failed to add product. Please ensure the form is valid.'
+                'Failed to add product. Please make sure that the form is valid.'
                 )
     else:
         form = ProductForm()
@@ -91,6 +91,33 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your product was successfully updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request,
+                'Failed to update product. Please make sure that the form is valid.'
+                )
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
