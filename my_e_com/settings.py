cfc7a1f8435854a24b39e36bdd6a73550f03cf81
gaspar91 <andreiafg.91@gmail.com>
2021-01-18 13:15:27 +0000

    created the heroku app, installed psyco and dj_django

--- my_e_com/settings.py
+++ my_e_com/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +124,7 @@ WSGI_APPLICATION = 'my_e_com.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
