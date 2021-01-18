ebdd701d6a8d1adbce5c729732a38fc96e5568b5
gaspar91 <andreiafg.91@gmail.com>
2021-01-18 13:49:14 +0000

    installed gunicorn and logged in heroku

--- my_e_com/settings.py
+++ my_e_com/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'c17l$@sfc&=6qs=5tuyv2$6n2u)ve88mtf_ja)o6$n0ee*ar^%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['planties.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'my_e_com.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
