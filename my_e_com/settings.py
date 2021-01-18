c309463899a398fc2d86ab561701300c55f4b72b
gaspar91 <andreiafg.91@gmail.com>
2021-01-18 14:37:02 +0000

    removed secret key and set debug

--- my_e_com/settings.py
+++ my_e_com/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c17l$@sfc&=6qs=5tuyv2$6n2u)ve88mtf_ja)o6$n0ee*ar^%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['planties.herokuapp.com', 'localhost']
 
