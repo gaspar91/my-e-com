8e7e9fc6a9f1649563bcae6715d0929dc5842fcc
gaspar91 <andreiafg.91@gmail.com>
2021-01-19 14:06:14 +0000

    fixed some issues with whitenoise

--- my_e_com/settings.py
+++ my_e_com/settings.py
@@ -180,8 +180,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # whitenoise
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
