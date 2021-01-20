be04a9c7f2c5cf0d440a6b88ceca10ca516777f0
gaspar91 <andreiafg.91@gmail.com>
2021-01-20 10:02:17 +0000

    fixing minor issues to setting.py

--- my_e_com/settings.py
+++ my_e_com/settings.py
@@ -186,7 +186,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = "static/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
